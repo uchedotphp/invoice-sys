9b7db1d7b9e4d21a0fda44d6876c6b15d1c9b1cd
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-06-02 10:17:06 +0100

    update: Main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -5,19 +5,60 @@ import ChevronLeft from "../../public/left-chevron.svg";
 import Header from "@components/Header";
 import AddIcon from "../../public/plus.svg";
 import ChevronDown from "../../public/down-chevron.svg";
+import CreateInvoice from "@components/invoices/Create";
+import { useState } from "react";
+import { InvoiceManagementType } from "@utils/schema";
 
 function MainLayout() {
+  const [invoicePanel, togglePanel] = useState<{
+    show: boolean;
+    invoiceType: InvoiceManagementType;
+  }>({
+    show: false,
+    invoiceType: "create",
+  });
+
+  const toggleInvoicePanel = ({
+    invoiceType = "create",
+    isVisible,
+  }: {
+    invoiceType?: InvoiceManagementType;
+    isVisible: boolean;
+  }) => {
+    togglePanel({
+      show: isVisible,
+      invoiceType,
+    });
+  };
+
   const goBack = useNavigate();
   const location = useLocation();
   const isIndexHome = location.pathname === "/";
 
   return (
-    <div className="bg-quinary h-full flex flex-col lg:flex-row">
+    // pull out panel
+    <aside className="bg-quinary h-full flex flex-col lg:flex-row">
+      <div
+        onClick={() => toggleInvoicePanel({ isVisible: false })}
+        className={`fixed bg-quaternary/50 w-full h-full z-0, [ ${
+          invoicePanel.show ? "block" : "hidden"
+        } ]`}
+      >
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          className="bg-quinary w-[719px] h-full pl-[159px] pt-[59px] pr-[56px pb-8 rounded-tr-[20px] rounded-br-[20px]"
+        >
+          <CreateInvoice type={invoicePanel.invoiceType} />
+        </div>
+      </div>
+
       {/* sidebar */}
       <Sidebar />
 
-      {/* Header area */}
-      <div className="mx-6 max-w-[730px] md:w-full md:mx-auto h-full mt-8 lg:mt-[77px] lg:mb-[54px] overflow-y-hidden">
+      <div className="mx-6 max-w-[730px] md:w-full md:mx-auto mt-8 lg:mt-[77px] lg:mb-[54px] overflow-y-hidden">
+        {/* Header area */}
         {!isIndexHome ? (
           <Button onClick={() => goBack(-1)} className="btn btn--flat pb-8">
             <img src={ChevronLeft} alt="chevron left" className="pr-6" />
@@ -43,7 +84,12 @@ function MainLayout() {
                 <img src={ChevronDown} alt="chevron down" className="ml-3.5" />
               </Button>
 
-              <Button onClick={()=> console.log('clicked')} className="btn btn--primary btn--rounded p-2 pr-[17px] space-x-4">
+              <Button
+                onClick={() =>
+                  toggleInvoicePanel({ invoiceType: "create", isVisible: true })
+                }
+                className="btn btn--primary btn--rounded p-2 pr-[17px] space-x-4"
+              >
                 <span className="bg-white text-primary rounded-full px-[10.98px] py-[11.02px]">
                   <img src={AddIcon} alt="add icon" />
                 </span>
@@ -55,12 +101,12 @@ function MainLayout() {
           </Header>
         )}
 
-        <main className="pt-16">
+        <main className="pt-16 h-full overflow-y-auto">
           {/* pages */}
           <Outlet />
         </main>
       </div>
-    </div>
+    </aside>
   );
 }
 
